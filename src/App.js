918fb3c8ad28bac01cd20019339ea77af1cc9023
Mani vardhan <manivardhan12356@gmail.com>
2023-11-27 06:48:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import NavScrollExample from "./components/Navbar";
+import Songs from "./components/Songs";
+import axios from 'axios';
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Make a GET request to your Flask API
+    axios.get('https://python-songs-api.onrender.com/')
+      .then(response => {
+        // Update the state with the fetched data
+        setSongs(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
+  // Filter songs based on search term
+  const filteredSongs = songs.filter(song =>
+    song.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    song.artist.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    song.album.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <NavScrollExample setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+      <Songs filteredSongs={filteredSongs } />
+      
     </div>
   );
 }
