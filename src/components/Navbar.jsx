dd50f49055e89cf9d49ba038c14ae9c38b40310a
Mani vardhan <manivardhan12356@gmail.com>
2023-11-30 17:32:03 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+function NavScrollExample({ searchTerm, handleSearchTermChange }) {
    return (
       <Navbar expand="lg" className="bg-gray-100 border-b-[1px] border-gray-300">
          <Container fluid>
@@ -17,21 +17,7 @@ function NavScrollExample() {
                   navbarScroll
 
                >
-                  {/* <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown title="Link" id="navbarScrollingDropdown">
-                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                     <NavDropdown.Item href="#action4">
-                        Another action
-                     </NavDropdown.Item>
-                     <NavDropdown.Divider />
-                     <NavDropdown.Item href="#action5">
-                        Something else here
-                     </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link href="#" disabled>
-                     Link
-                  </Nav.Link> */}
+                 
                </Nav>
                <Form className="d-flex">
                   <Form.Control
@@ -39,6 +25,8 @@ function NavScrollExample() {
                      placeholder="Search"
                      className="me-2"
                      aria-label="Search"
+                     value={searchTerm}
+                     onChange={handleSearchTermChange}
                   />
                </Form>
             </Navbar.Collapse>
